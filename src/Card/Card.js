f52168a32e584524e356c5e2453fe9367077bdf5
Kreker <yahor.flame@gmail.com>
2022-04-28 19:57:50 +0200

    response

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -5,23 +5,28 @@ import  API_KEY from "../settings";
 const Card = ({ city }) => {
     const [data, setData] = useState(null);
     useEffect( () => {
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`)
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
         .then(res => res.json())
         .then(setData);
-    }, []);
+    }, [city]);
     console.log('data', data);
+    if (!data) return null;
+
+    const { name, weather, main } = data;
+    const { description, icon } = weather[0];
+    const { temp, humidity, feels_like} = main;
 
     return (
         <div className="card-list">
             <div className="card-info">
-                <img className="card-icon" src="https://openweathermap.org/img/wn/10d@2x.png" alt="icon"/>
-                <div className="title">{city}</div>
-                <div className="description">Cloudy</div>
-                <div className="temperature">20</div>
+                <img className="card-icon" src={`https://openweathermap.org/img/wn/${icon}@2x.png`} alt="icon"/>
+                <div className="title">{name}</div>
+                <div className="description">{description}</div>
+                <div className="temperature">{temp.toFixed()}&deg;</div>
             </div>
             <div className="additional-info">
-                <div>Humidity: 15</div>
-                <div>Feels like: 19</div>
+                <div>Humidity: {humidity}</div>
+                <div>Feels like: {feels_like.toFixed()}&deg;</div>
             </div>
         </div>
     );
